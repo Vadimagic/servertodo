988e8e8af4855146e601f865321748688fd9cf8d
Vadim Bida <vadim.prog.2000@gmail.com>
2023-03-20 14:10:48 +1000

    change settings cookie

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,7 +19,9 @@ class UserController {
 			const userData = await userService.registration(name, password, role)
 			res.cookie('refreshToken', userData.refreshToken, {
 				maxAge: 30 * 24 * 60 * 60 * 1000,
-				httpOnly: true
+				httpOnly: true,
+				sameSite: 'none',
+				secure: true
 			})
 			return res.json(userData)
 		} catch (e) {
@@ -38,7 +40,9 @@ class UserController {
 			const userData = await userService.login(name, password)
 			res.cookie('refreshToken', userData.refreshToken, {
 				maxAge: 30 * 24 * 60 * 60 * 1000,
-				httpOnly: true
+				httpOnly: true,
+				sameSite: 'none',
+				secure: true
 			})
 			return res.json(userData)
 		} catch (e) {
@@ -75,7 +79,9 @@ class UserController {
 			const userData = await userService.refresh(refreshToken)
 			res.cookie('refreshToken', userData.refreshToken, {
 				maxAge: 30 * 24 * 60 * 60 * 1000,
-				httpOnly: true
+				httpOnly: true,
+				sameSite: 'none',
+				secure: true
 			})
 			return res.json(userData)
 		} catch (e) {
