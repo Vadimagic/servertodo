3c5b4a463da419dc668338a02132f663b6a91b6b
Vadim Bida <vadim.prog.2000@gmail.com>
2023-03-20 13:51:13 +1000

    remove morgan

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import express from 'express'
-import morgan from 'morgan'
 
+// import morgan from 'morgan'
 import { errorHandler, notFound } from './middlewares/error.middleware.js'
 import { prisma } from './prisma/prisma.js'
 import todoRoutes from './router/todo.routes.js'
@@ -14,9 +14,9 @@ dotenv.config()
 const app = express()
 
 async function main() {
-	if (process.env.NODE_ENV === 'development') {
-		app.use(morgan('dev'))
-	}
+	// if (process.env.NODE_ENV === 'development') {
+	// 	app.use(morgan('dev'))
+	// }
 	app.use(
 		cors({
 			credentials: true,
